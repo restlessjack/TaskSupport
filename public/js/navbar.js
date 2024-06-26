122a2424c037ab898cbc1bb4d6847232d7532454
restlessjack <102357767+restlessjack@users.noreply.github.com>
2024-06-26 03:17:52 +0100

    more navbar tests on student side

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,5 +1,18 @@
-// navbar.js
-document.querySelector(".hamburger").addEventListener("click", function(){
-    document.querySelector(".wrapper").classList.toggle("collapse");
- });
- 
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".hamburger").addEventListener("click", function(){
+        document.querySelector(".wrapper").classList.toggle("collapse");
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const currentUrl = window.location.pathname;
+    const sidebarLinks = document.querySelectorAll(".sidebar-link");
+
+    sidebarLinks.forEach(link => {
+        if (link.getAttribute("href") === currentUrl) {
+            link.classList.add("active");
+        }
+    });
+});
